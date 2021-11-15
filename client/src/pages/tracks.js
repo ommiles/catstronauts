34ec5312343a633777e6eadd062e13136841ab7b
O.M. Miles <73572370+ommiles@users.noreply.github.com>
2021-11-14 19:34:58 -0800

    add loading spinner and cards ui

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Layout } from '../components';
-import { gql } from '@apollo/client';
+import { useQuery, gql } from '@apollo/client';
+import TrackCard from '../containers/track-card';
+import { Layout, QueryResult } from '../components';
 
 const TRACKS = gql`
   query ExampleQuery {
@@ -24,7 +25,16 @@ const TRACKS = gql`
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map((track, index) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
