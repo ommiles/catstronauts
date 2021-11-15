e03a15bf2a1057bbbaedf8f7ec76576d09f9342a
O.M. Miles <73572370+ommiles@users.noreply.github.com>
2021-11-14 19:23:48 -0800

    add gql query

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import { Layout } from '../components';
+import { gql } from '@apollo/client';
+
+const TRACKS = gql`
+  query ExampleQuery {
+    tracksForHome {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+    }
+  }
+`;
 
 /**
  * Tracks Page is the Catstronauts home page.
